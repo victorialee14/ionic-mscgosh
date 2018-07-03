1b04b16a670b0e0cd37127841867e97cb6a53419
victorialee <victoria.lee.14@ucl.ac.uk>
2018-07-03 22:20:34 +0100

    Bar chart added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Chart } from 'chart.js';
 
 import 'chartjs-plugin-streaming';
 
@@ -22,11 +23,45 @@ export class HomePage {
       }]
     }
   };
+ 
+    constructor(public navCtrl: NavController) {
+ 
+    }
+ 
+    public barChartOptions:any = {
+  scaleShowVerticalLines: false,
+  responsive: true
+};
+public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+public barChartType:string = 'bar';
+public barChartLegend:boolean = true;
 
+public barChartData:any[] = [
+  {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+  {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+];
 
-  constructor(public navCtrl: NavController) {
+// events
+public chartClicked(e:any):void {
+  console.log(e);
+}
 
+public chartHovered(e:any):void {
+  console.log(e);
+}
 
-  }
-
+public randomize():void {
+  // Only Change 3 values
+  let data = [
+    Math.round(Math.random() * 100),
+    59,
+    80,
+    (Math.random() * 100),
+    56,
+    (Math.random() * 100),
+    40];
+  let clone = JSON.parse(JSON.stringify(this.barChartData));
+  clone[0].data = data;
+  this.barChartData = clone;
+}
 }
